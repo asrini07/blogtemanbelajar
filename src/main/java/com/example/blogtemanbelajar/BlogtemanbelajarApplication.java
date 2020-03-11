59739c59ea4672ae229ae973393f8572ee2c7008
Aas Asrini <aasasrini@Aass-MacBook-Air.local>
2020-03-11 16:46:03 +0700

    first commit

---
+++ src/main/java/com/example/blogtemanbelajar/BlogtemanbelajarApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogtemanbelajar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogtemanbelajarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogtemanbelajarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blogtemanbelajar/BlogtemanbelajarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogtemanbelajar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogtemanbelajarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
