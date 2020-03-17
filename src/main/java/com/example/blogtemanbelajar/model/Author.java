ef7a7e7291dc716e4c6ae5ec26e315274b348c8d
Aas Asrini <asrinirpl07@gmail.com>
2020-03-17 17:37:43 +0700

    model author

---
+++ src/main/java/com/example/blogtemanbelajar/model/Author.java
@@ -0,0 +1,67 @@
+package com.example.blogtemanbelajar.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Author extends AuditModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 45)
+    private String first_name;
+    
+    @Column(length = 45)
+    private String last_name;
+
+    @Column(length = 45)
+    private String username;
+
+    @Column(length = 150)
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return first_name;
+    }
+
+    public void setFirstName(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLastName() {
+        return last_name;
+    }
+
+    public void setLastName(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
